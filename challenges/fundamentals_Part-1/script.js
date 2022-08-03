5057c561f7a665128181970b269fd9ce99714808
Proshanto <proshantolal3@gmail.com>
2022-08-03 23:25:14 +0600

    Modified fundamentals_Part-1/script.js file
    Did all the practice from the course part 1 - 007. Learned about variables & naming convention for variables.

--- challenges/fundamentals_Part-1/script.js
+++ challenges/fundamentals_Part-1/script.js
@@ -1,6 +1,25 @@
 let js = "amazing";
-if (js === "amazing") {
-  alert("Javascript is FUN!");
-}
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Proshanto");
+console.log(27);
+
+let firstName = "Diptey";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let proshanto_lal = "PM";
+let $function = 69;
+
+let person = "Proshanto";
+let PI = 3.14159;
+
+let myFirstJob = "programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
