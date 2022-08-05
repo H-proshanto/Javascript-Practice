5286aa14ef67faab481b80bd46c1e68c6a5bf38e
Proshanto <proshantolal3@gmail.com>
2022-08-05 22:13:41 +0600

    Modifies script.js
    Practiced type coercion and type conversion from the course part 1 - 016

--- challenges/fundamentals_Part-1/script.js
+++ challenges/fundamentals_Part-1/script.js
@@ -192,3 +192,20 @@ else {
 }
 */
 
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(inputYear + 18);
+
+console.log(Number("Proshanto"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//type coersion
+console.log("I am" + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" * "2");
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
