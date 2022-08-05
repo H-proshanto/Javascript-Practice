afd583fef4c880bdc903d1861e8ea5c8775bf010
Proshanto <proshantolal3@gmail.com>
2022-08-05 22:45:12 +0600

    Modifies script.js
    practiced if else with equality operators part 1 - 017

--- challenges/fundamentals_Part-1/script.js
+++ challenges/fundamentals_Part-1/script.js
@@ -209,7 +209,7 @@ console.log("23" * "2");
 let n = "1" + 1;
 n = n - 1;
 console.log(n);
-*/
+
 
 //five falsy values:0, '', undefined, null, NaN
 console.log(Boolean(0));
@@ -233,4 +233,31 @@ if(height) {
 }
 else {
     console.log('Height is UNDEFINED');
+}
+*/
+
+const age = "18";
+if (age === 18) {
+  console.log("You just became an adult :D (strict)");
+}
+if (age == 18) {
+  console.log("You just became an adult :D (loose)");
+}
+
+const favourite = Number(prompt("What is your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number");
+} else if (favourite === 7) {
+  console.log("Cool! 7 is an amazing number");
+} else if (favourite === 9) {
+  console.log("Cool! 9 is an amazing number");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) {
+    console.log('Why not 23?');
 }
