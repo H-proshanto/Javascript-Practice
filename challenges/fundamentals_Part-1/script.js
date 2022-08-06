89c8d832ae751ea1437d03d6977f6df87f9fb86e
Proshanto <proshantolal3@gmail.com>
2022-08-06 18:37:50 +0600

    Practiced Ternery operator

--- challenges/fundamentals_Part-1/script.js
+++ challenges/fundamentals_Part-1/script.js
@@ -331,7 +331,7 @@ if(isDolphinsWinner && isDolphinsRequiredMinScore) {
 } else {
   console.log("No one wins :(");
 }
-*/
+
 
 const day = "monday";
 
@@ -372,3 +372,23 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day");
 }
+
+*/
+
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine")
+  : console.log("I like to drink water");
+
+const drink = age >= 18 ? "Wine" : "Water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "Wine" : "Water"}`);
