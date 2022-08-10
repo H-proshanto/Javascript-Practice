d924c104d3ccc5bf69c7834de9fa3c0c46a825b7
Proshanto <proshantolal3@gmail.com>
2022-08-10 18:04:55 +0600

    Did coding challenge #4 from fundamentals part 2

--- challenges/Fundamentals-Part-2/script.js
+++ challenges/Fundamentals-Part-2/script.js
@@ -473,7 +473,7 @@ for(let exercise = 1; exercise < 4; exercise++) {
   }
 
 }
-*/
+
 
 // Part 2 - 019
 for(let rep = 1; rep < 6; rep++) {
@@ -496,3 +496,32 @@ while (dice !== 6) {
     console.log(`Loop is about to end...`);
   }
 }
+
+*/
+const calcTip = function (bill) {
+  return bill >= 50 && bill <=300 ? bill * 0.15 : bill *0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+console.log(calcAverage(2, 3, 6));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
