07456c84fd8a98686057a981a559f7a6fbcb8ec7
Proshanto <proshantolal3@gmail.com>
2022-08-14 02:35:53 +0600

    Did the coding challenge #2 from from closer look at functions from the course

--- Functions/script.js
+++ Functions/script.js
@@ -323,3 +323,13 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000;
 boardPassengers(180, 3);
 */
+// Challenge #2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
