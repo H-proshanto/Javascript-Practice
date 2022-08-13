4c1922c8083bc96a0b729c3e5a509b42bbd0d913
Proshanto <proshantolal3@gmail.com>
2022-08-13 21:20:53 +0600

    Did coding challenge #3 from the data structures and modern operators module from the course

--- Data Structers and Modern operators/script.js
+++ Data Structers and Modern operators/script.js
@@ -590,3 +590,40 @@ console.log(...question.entries());
 console.log(...question.keys());
 console.log(...question.values());
 */
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1.
+const events = [...new Set([...gameEvents.values()])];
+console.log(events);
+
+//2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3.
+console.log(
+  `An event happend on average every ${90 / gameEvents.size} minutes`
+);
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+//4.
+for (const [key, value] of gameEvents) {
+  const half = key > 45 ? 'SECOND HALF' : 'First HALF';
+  console.log(`[${half}] ${key}: ${value}`);
+}
