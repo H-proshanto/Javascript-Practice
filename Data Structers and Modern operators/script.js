3ae86e902c1bbf53407c6dc650acd23647dd55d1
Proshanto <proshantolal3@gmail.com>
2022-08-13 23:59:45 +0600

    Completed data structure and modern operators from the course

--- Data Structers and Modern operators/script.js
+++ Data Structers and Modern operators/script.js
@@ -1,8 +1,6 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const openingHours = {
   thu: {
@@ -775,7 +773,7 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
-*/
+
 
 // Challenge #4
 document.body.append(document.createElement('textarea'));
@@ -795,3 +793,19 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(output.padEnd(20, ' ').padEnd(20 + i + 1, '✅'));
   }
 });
+*/
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+
+  console.log(output);
+}
