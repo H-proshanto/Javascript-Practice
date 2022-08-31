60d2c529a91dd2cb7a49b65d90bcd62df137ac16
Proshanto <proshantolal3@gmail.com>
2022-08-31 22:10:04 +0600

    Fixes bug file names were not smaller

--- Forkify-App/src/js/config.js
+++ Forkify-App/src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '199458c5-674c-4db4-a068-b54aa45d24f8';
-export const MODAL_CLOSE_SEC = 2;
+export const MODAL_CLOSE_SEC = 1.2;
